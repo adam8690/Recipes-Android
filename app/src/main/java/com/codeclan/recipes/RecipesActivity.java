ce58d6cb180519ce64c1283e547e0c3a3f5dd27c
Adam <adam8690@hotmail.co.uk>
2017-03-21 15:38:53 +0000

    Listview working and returning recipe objects on click via logging.

---
+++ app/src/main/java/com/codeclan/recipes/RecipesActivity.java
@@ -0,0 +1,66 @@
+package com.codeclan.recipes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class RecipesActivity extends AppCompatActivity {
+
+    ArrayList<Recipe> recipes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.recipes_list);
+
+        initializeData();
+
+        RecipesAdapter recipesAdapter = new RecipesAdapter(this, recipes);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(recipesAdapter);
+
+    }
+
+    public void initializeData(){
+        ArrayList<Ingredient> ingreds = new ArrayList<Ingredient>();
+
+
+
+        ingreds.add(new Ingredient("thing"));
+
+        recipes.add(new Recipe("Test Recipe 1", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 2", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 3", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 4", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 5", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 6", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 7", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 8", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 9", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 10", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 11", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 12", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 13", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+        recipes.add(new Recipe("Test Recipe 14", "do stuff", ingreds, R.mipmap.recipe_placeholder_colour));
+    }
+
+
+    public void nameClicked(View textView){
+
+        Recipe r = (Recipe)textView.getTag();
+
+        Log.d("Name clicked", r.getName());
+    }
+
+
+
+}
---
+++ app/src/main/java/com/codeclan/recipes/RecipesAdapter.java
@@ -0,0 +1,37 @@
+package com.codeclan.recipes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Adam Baxter on 21/03/2017.
+ */
+
+public class RecipesAdapter extends ArrayAdapter<Recipe>{
+
+    public RecipesAdapter(Context context, ArrayList<Recipe> recipes){
+        super(context, 0, recipes);
+    }
+
+    @Override
+    public View getView(int position, View listItemView, ViewGroup parent) {
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.recipe_item, parent, false);
+        }
+
+        Recipe currentRecipe = getItem(position);
+
+        TextView recipeName = (TextView)listItemView.findViewById(R.id.recipe_name);
+        recipeName.setText(currentRecipe.getName());
+        recipeName.setTag(currentRecipe);
+
+        return listItemView;
+    }
+
+}
