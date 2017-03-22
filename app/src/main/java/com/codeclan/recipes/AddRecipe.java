82dd9b4e32f4e66a3323488c27cd7cd3ff203d8a
Adam <adam8690@hotmail.co.uk>
2017-03-22 14:14:42 +0000

    added a save button to add recipe page which for now only takes you back to the recipes list.

--- app/src/main/java/com/codeclan/recipes/AddRecipe.java
+++ app/src/main/java/com/codeclan/recipes/AddRecipe.java
@@ -1,7 +1,14 @@
 package com.codeclan.recipes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class AddRecipe extends AppCompatActivity {
 
@@ -9,5 +16,49 @@ public class AddRecipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+
+        //          Creating AppBar and toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent intent;
+
+        switch(item.getItemId()) {
+            case R.id.action_add_recipe:
+                intent = new Intent(this, AddRecipe.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.action_list:
+                intent = new Intent(this, RecipesActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    public void onSaveButtonClicked(View view){
+        Log.d("Button pressed", "Save button");
+
+//        Recipe recipe = new Recipe(name, method, ingredients, photoid);
+
+
+
+//        take user back to recipes list.
+        Intent intent = new Intent(this, RecipesActivity.class);
+        startActivity(intent);
+    }
+
 }
