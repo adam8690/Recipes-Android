1b8483463af1b3f0c39e48003a7a2858f0026094
Adam <adam8690@hotmail.co.uk>
2017-03-22 15:47:13 +0000

    pressing save on a recipe now creates a new recipe object and saves it to shared preferences.

--- app/src/main/java/com/codeclan/recipes/AddRecipe.java
+++ app/src/main/java/com/codeclan/recipes/AddRecipe.java
@@ -1,6 +1,8 @@
 package com.codeclan.recipes;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,8 +11,22 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
+import static com.codeclan.recipes.Ingredient.convertListStringToArrayList;
 
 public class AddRecipe extends AppCompatActivity {
+
+    public static final String RECIPES = "savedRecipes";
+    EditText inputRecipeName;
+    EditText inputMethod;
+    EditText inputIngredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +36,12 @@ public class AddRecipe extends AppCompatActivity {
         //          Creating AppBar and toolbar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+//        get text from input fields
+        inputRecipeName = (EditText) findViewById(R.id.enter_recipe_name);
+        inputIngredients = (EditText) findViewById(R.id.enter_ingredients);
+        inputMethod = (EditText) findViewById(R.id.enter_method);
+
     }
 
     @Override
@@ -52,9 +74,29 @@ public class AddRecipe extends AppCompatActivity {
     public void onSaveButtonClicked(View view){
         Log.d("Button pressed", "Save button");
 
-//        Recipe recipe = new Recipe(name, method, ingredients, photoid);
+        String name = inputRecipeName.getText().toString();
+        String method = inputMethod.getText().toString();
+        String ingredients = inputIngredients.getText().toString();
 
+//        need to create arraylist of ingredients. Using my ingredients method.
+        ArrayList<Ingredient> ingreds = convertListStringToArrayList(ingredients);
 
+//create new recipe object with input details
+        Recipe recipe = new Recipe(name, method, ingreds, R.drawable.recipe_placeholder);
+        Log.d("Saved Recipe Object nam", recipe.getName());
+        Log.d("Saved Recipe Object met", recipe.getMethod());
+        Log.d("Saved Recipe Object ing", recipe.getIngredients().get(0).getName());
+
+//   pass this object to shared preferences to be saved
+        SharedPreferences sharedPref = getSharedPreferences(RECIPES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        Gson gson = new Gson();
+
+        editor.putString("savedRecipes", gson.toJson(recipe));
+        editor.apply();
+//Recipe is saved but can I save multiple recipes and get them all back on starting the app?
+        Toast.makeText(this, "Recipe Saved!", Toast.LENGTH_LONG).show();
 
 //        take user back to recipes list.
         Intent intent = new Intent(this, RecipesActivity.class);
